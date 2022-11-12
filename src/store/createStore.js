cf8206af05c055ae2e9d37c79d140dd8ba12614a
den1aka1aki <lavrov.den@gmail.com>
2022-11-12 21:59:29 +0100

    goingOn_lesson_redux

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,11 +1,11 @@
-export function createStore (reducer, initialState){
+function configureStore (reducer, initialState){
     let state = initialState
-    let listeners = []
+    let listeners = [];
     function getState () {
         return state
     }
     function dispatch (action) {
-        state = refucer(state, action)
+        state = reducer(state, action)
         for (let i=0; i<listeners.length; i++){
             const listener = listeners[i]
             listener()
@@ -16,3 +16,4 @@ export function createStore (reducer, initialState){
     }
     return {getState, dispatch, subscribe}
 }
+export default configureStore;
---
+++ src/store/middleware/thunk.js
@@ -0,0 +1,13 @@
+
+export function thunk({getState, dispatch}) {
+    return function wrapDispatch(next) {
+        return function handleAction(action) {
+            if (typeof action === 'function'){
+                action(dispatch, getState);
+            }else{
+            console.log(typeof action)
+            return next(action)
+            }
+        }
+    }
+}
